67d60f04eafc6b4aaf70542b3325c115006c134b
shubhams <shubhams@legal.regn.net>
2025-01-15 11:22:28 -0500

    linkedin scraping complete

--- ML Projects/Udemy_Eden/ice_breaker.py
+++ ML Projects/Udemy_Eden/ice_breaker.py
@@ -3,6 +3,10 @@ from dotenv import load_dotenv
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
+
+from third_parties.linkedin import scrape_linkedin_profile
+
+
 def print_hi(name):
     print(f"Hi, {name}")
 
@@ -16,12 +20,16 @@ if __name__ == "__main__":
     print_hi("PyCharm")
     load_dotenv()
     summary_template = """
-        given the information {information} about a person from I want you to create:
+        given the LinkedIn information {information} about a person from I want you to create:
         1. a short summary
         2. two interesting facts about them
     """
     summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    llm = ChatOpenAI(temperature=1, model_name="gpt-3.5-turbo")
     chain = summary_prompt_template | llm | StrOutputParser()
-    res = chain.invoke(input={"information": information})
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url="https://www.linkedin.com/in/shubham-jain-b31375137/",
+        mock=True
+    )
+    res = chain.invoke(input={"information": linkedin_data})
     print(res)
