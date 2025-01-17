8f3326c92cf521ae203a67070fa2adac9a67998f
shubhams <shubhams@legal.regn.net>
2025-01-17 13:42:19 -0500

    add output parser

--- ML Projects/Udemy_Eden/ice_breaker.py
+++ ML Projects/Udemy_Eden/ice_breaker.py
@@ -6,6 +6,7 @@ from langchain_openai import ChatOpenAI
 
 from agents.linkedin_lookup_agent import lookup
 from agents.twitter_lookup_agent import lookup_twitter
+from output_parsers import summary_parser
 from third_parties.linkedin import scrape_linkedin_profile
 from third_parties.twitter import scrape_user_tweets
 
@@ -14,25 +15,30 @@ def ice_break_with(name: str) -> str:
     linkedin_url = lookup(name) # uses agent to find the right LinkedIn URL
     linkedin_data = scrape_linkedin_profile(    # uses API to find the data on LinkedIn Profile
         linkedin_profile_url=linkedin_url,
-        mock=True #set mock=True to fix the search
+        mock=False #set mock=True to fix the search
     )
     twitter_username = lookup_twitter(name) # uses agent to find the right Twiiter Username
     twitter_data = scrape_user_tweets(    # uses API to find the tweets on Twitter Profile
         username=twitter_username,
-        mock=True #set mock=True to fix the search
+        mock=False #set mock=True to fix the search
     )
     summary_template = """
             given the LinkedIn information {information} and twitter posts {twitter_posts} about a person from I want you to create:
             1. a short summary
             2. two interesting facts about them
             Use the data from Linkedin and Twitter
+            \n{format_instructions}
         """
-    summary_prompt_template = PromptTemplate(input_variables=["information", "twitter_posts"], template=summary_template)
+    summary_prompt_template = PromptTemplate(
+                                            input_variables=["information", "twitter_posts"],
+                                            partial_variables={"format_instructions":summary_parser.get_format_instructions()},
+                                            template=summary_template
+                                            )
     llm = ChatOpenAI(temperature=1, model_name="gpt-4o-mini")
-    chain = summary_prompt_template | llm | StrOutputParser()
-
+    # chain = summary_prompt_template | llm | StrOutputParser()
+    chain = summary_prompt_template | llm | summary_parser
     res = chain.invoke(input={"information": linkedin_data, "twitter_posts": twitter_data})
-    print("Data data about the user:")
+    print("Data about the user:")
     print(res)
 
 if __name__ == "__main__":
