d0f69b379b6028b1e0bee4a6ad4a2be55b7f2275
ProboticsX <jainshubham0403@gmail.com>
2025-05-10 20:54:15 -0400

    add podcast code (not working)

--- ML Projects/Langgraph-Practice/15-news-summarizer/graph.py
+++ ML Projects/Langgraph-Practice/15-news-summarizer/graph.py
@@ -1,5 +1,4 @@
-
-from functions import news_supervisor_agent, top_headlines_node, top_headlines_supervisor_agent, top_headlines_summarizer_node
+from functions import news_supervisor_agent, top_headlines_node, top_headlines_supervisor_agent, top_headlines_summarizer_node, podcast_supervisor_agent, podcast_transcript_generator_node
 from constants import *
 from common_imports import *
 from classes import AgentState
@@ -7,11 +6,13 @@ from classes import AgentState
 workflow = StateGraph(AgentState)
 
 workflow.add_node(NEWS_SUPERVISOR_AGENT, news_supervisor_agent)
+
 workflow.add_node(TOP_HEADLINES_SUPERVISOR_AGENT, top_headlines_supervisor_agent)
 workflow.add_node(TOP_HEADLINES_SUMMARIZER_AGENT, top_headlines_summarizer_node)
 workflow.add_node(TOP_HEADLINES_AGENT, top_headlines_node)
-# workflow.add_node(PODCAST_SUPERVISOR_AGENT, podcast_supervisor_agent)
-# workflow.add_node(PODCAST_TRANSCRIPT_GENERATOR_AGENT, podcast_transcript_generator_node)
+
+workflow.add_node(PODCAST_SUPERVISOR_AGENT, podcast_supervisor_agent)
+workflow.add_node(PODCAST_TRANSCRIPT_GENERATOR_AGENT, podcast_transcript_generator_node)
 # workflow.add_node(PODCAST_AUDIO_GENERATOR_AGENT, podcast_audio_generator_node)
 
 
