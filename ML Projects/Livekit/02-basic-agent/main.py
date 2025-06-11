3699af036c7e6edcd170b9e0a298ee7806eb5c1c
ProboticsX <jainshubham0403@gmail.com>
2025-06-11 18:14:18 -0400

    push changes in voice agent

--- ML Projects/Livekit/02-basic-agent/main.py
+++ ML Projects/Livekit/02-basic-agent/main.py
@@ -73,7 +73,7 @@ class Assistant(Agent):
             
         Returns:
             str: Answer to the user's question""" 
-        date = "2025-06-05"#await self.get_todays_date()
+        date = await self.get_todays_date()
         headlines = await self.get_relevant_headlines(query=query, top_k=1, date=date)
         
         # RAG Grader Agent
@@ -270,32 +270,32 @@ async def entrypoint(ctx: agents.JobContext):
     #     summary = usage_collector.get_summary()
     #     logger.info(f"Usage: {summary}")
 
-    inactivity_task: asyncio.Task | None = None
+    # inactivity_task: asyncio.Task | None = None
 
-    async def user_presence_task():
-        # try to ping the user 3 times, if we get no answer, close the session
-        for _ in range(3):
-            await session.generate_reply(
-                instructions=(
-                    "The user has been inactive. Politely check if the user is still present, and "
-                    "gently guide the conversation back toward your intended goal."
-                )
-            )
-            await asyncio.sleep(10)
+    # async def user_presence_task():
+    #     # try to ping the user 3 times, if we get no answer, close the session
+    #     for _ in range(3):
+    #         await session.generate_reply(
+    #             instructions=(
+    #                 "The user has been inactive. Politely check if the user is still present, and "
+    #                 "gently guide the conversation back toward your intended goal."
+    #             )
+    #         )
+    #         await asyncio.sleep(10)
 
-        await asyncio.shield(session.aclose())
-        ctx.delete_room()
+    #     await asyncio.shield(session.aclose())
+    #     ctx.delete_room()
 
-    @session.on("user_state_changed")
-    def _user_state_changed(ev: UserStateChangedEvent):
-        nonlocal inactivity_task
-        if ev.new_state == "away":
-            inactivity_task = asyncio.create_task(user_presence_task())
-            return
+    # @session.on("user_state_changed")
+    # def _user_state_changed(ev: UserStateChangedEvent):
+    #     nonlocal inactivity_task
+    #     if ev.new_state == "away":
+    #         inactivity_task = asyncio.create_task(user_presence_task())
+    #         return
 
-        # ev.new_state: listening, speaking, ..
-        if inactivity_task is not None:
-            inactivity_task.cancel()
+    #     # ev.new_state: listening, speaking, ..
+    #     if inactivity_task is not None:
+    #         inactivity_task.cancel()
 
     # shutdown callbacks are triggered when the session is over
     # ctx.add_shutdown_callback()
---
+++ ML Projects/Livekit/generate-token/generate_token.py
@@ -0,0 +1,49 @@
+from dotenv import load_dotenv
+from typing import Any, List, Literal, Dict, Optional
+from livekit import agents
+from livekit.agents import AgentSession, Agent, RoomInputOptions, function_tool, RunContext, metrics, UserStateChangedEvent, JobProcess
+from livekit.plugins import (
+    openai,
+    silero,
+    google,
+)
+from livekit.plugins.turn_detector.multilingual import MultilingualModel
+from livekit.agents.voice import MetricsCollectedEvent
+from pinecone import Pinecone
+import os
+import logging
+import asyncio
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langgraph.prebuilt import create_react_agent, ToolNode
+from datetime import datetime
+from typing_extensions import TypedDict
+import requests
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+load_dotenv()
+
+from livekit import api
+from flask import Flask
+
+app = Flask(__name__)
+ROOM_ID = "playground-0vsH-cHSw"
+PARTICIPANT_ID = "identity-gJUq"
+
+@app.route('/getToken')
+def getToken():
+  token = api.AccessToken(os.getenv('LIVEKIT_API_KEY'), os.getenv('LIVEKIT_API_SECRET')) \
+    .with_identity(f"{PARTICIPANT_ID}") \
+    .with_name(f"{ROOM_ID}") \
+    .with_grants(api.VideoGrants(
+        room_join=True,
+        room=f"{ROOM_ID}",
+    ))
+  token_str = token.to_jwt()
+  print(f"Generated token: {token_str}")
+  return token_str
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+
